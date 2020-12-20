67ca86a37d16b7d5cbb22443ed738db475759899
Tom Berman <tjcberman@gmail.com>
2020-12-20 11:40:11 +0000

    Add show and hide replies

--- lib/bkclb/build_tree.ex
+++ lib/bkclb/build_tree.ex
@@ -4,6 +4,11 @@ defmodule BuildTree do
     |> Enum.reduce([], fn msg, acc -> calc_depth(msg, acc) end)
     |> Enum.map(fn msg -> Map.put(msg, :ultimate_parent, get_ultimate_parent_id(msg, msgs)) end)
     |> Enum.sort(&(&1.ultimate_parent <= &2.ultimate_parent))
+    |> Enum.map(fn msg -> Map.put(msg, :has_children, has_children?(msg, msgs)) end)
+  end
+
+  def has_children?(msg, all) do
+    Enum.any?(all, fn x -> x.parent_id == msg.id end)
   end
 
   def calc_depth(msg, acc) do
